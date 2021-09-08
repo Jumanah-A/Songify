5daf1ec3ab72c3d7e7d164dae5ecae248ba8aa3b
Jumanah Almajnouni <jumanahh@outlook.com>
2021-09-08 01:25:16 +0000

    write out the routing methods must set up an initialize passport strategy

--- server/index.js
+++ server/index.js
@@ -1,9 +1,44 @@
 require('dotenv/config');
-const express = require('express');
 const errorMiddleware = require('./error-middleware');
 const staticMiddleware = require('./static-middleware');
+const express = require('express');
+// const session = require('express-session');
+const passport = require('passport');
+// const SpotifyStrategy = require('passport-spotify').Strategy;
+// const spotify = require('spotify');
 
+// const CLIENT_ID = process.env.CLIENT_ID;
+// const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const authCallbackPath = '/auth/spotify/callback';
 const app = express();
+
+// passport.use(new SpotifyStrategy(
+//   () => {}
+// ))
+
+// homepage redirect
+app.get('/');
+// spotify auth page redirect
+app.get('/auth/spotify', passport.authenticate('spotify',
+  {
+    scope: ['user-read-email', 'user-read-private'],
+    showDialog: true
+  }));
+
+// auth callback routing
+app.get(authCallbackPath,
+  passport.authenticate('spotify', { failureRedirect: '/' }),
+  (req, res) => {
+    res.redirect('/welcome');
+  }
+);
+
+// logout  routing
+app.get('/logout', (req, res) => {
+  req.logout();
+  // redirect to the homepage/root
+  res.redirect('/');
+});
 
 app.use(staticMiddleware);
 
